1c861cb1f4379022261812c46ceedca5736b9fb8
abscordida <nyancheraabscordia@gmail.com>
2023-06-15 08:21:15 +0300

    my first commit

--- hello.py
+++ hello.py
@@ -1,1 +1,32 @@
-print("hello abscordida")
+
+days = int(input("enter the number of days: "))
+
+hours = days *24
+minutes = hours *60
+seconds = minutes *60
+
+print("In",days,"days","there are ",hours,"hours",minutes,"minutes and ", seconds,"seconds" )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
